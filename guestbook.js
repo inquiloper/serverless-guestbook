c32ffa223b0b8c10e3ea1d9336298ca66d95a1a7
inquiloper <inquiloper@gmail.com>
2022-03-20 20:54:29 +0100

    Updated api url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://d24d4783.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
